d863eef10222907eb0f88535090f232506b9c9eb
Boleslav Březovský <rebolek@gmail.com>
2015-02-26 08:35:26 +0100

    new version

--- js/lest.js
+++ js/lest.js
@@ -3,15 +3,47 @@
  */
 
 
- function test() {
- 	txt = xml();
- 	alert(txt);
- }
+function test() {
+ 	reply = action("ping");
+ 	setContent("reply", reply);
+}
 
- function xml() {
+function action(act,data,target) {
+	/* TODO: Add support for data */
+	var reply = sendAction(act);
+	setContent(target,reply);
+}
+
+function sendAction(act) {
  	var reg = new XMLHttpRequest();
- 	reg.open("GET","index.r3?action=ping",false);
+ 	act = "index.r3?action=" + act
+ 	reg.open("GET", act, false);
 	reg.send();
 	res = reg.response; 
 	return res;
- }
+}
+
+function setContent(id, data) {
+	var elem = document.getElementById(id);
+	elem.innerHTML = data;
+}
+
+function getAttribute(id, attr) {
+	var elem = document.getElementById(id);
+	return elem[attr];
+}
+
+function setAttribute(id, attr, data) {
+	var elem = document.getElementById(id);
+	elem[attr] = data;
+}
+
+// watcher
+
+function watchFunc() {
+//	alert("Interval reached");
+	var reply = sendAction("ping","","");
+	console.debug(reply);
+}
+
+// var watcher = setInterval(watchFunc, 5000);
