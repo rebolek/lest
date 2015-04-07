b54ae2087ba1037509c761165f4ff8d94c3c5d33
Boleslav Březovský <rebolek@gmail.com>
2015-04-07 18:30:47 +0200

    newer version

--- js/lest.js
+++ js/lest.js
@@ -2,46 +2,62 @@
  * Lest.js
  */
 
+var lestWords = {};
 
-function test() {
- 	reply = action("ping");
- 	setContent("reply", reply);
+function setWord(word, value) {
+	lestWords[word] = value;
 }
 
-function debug(data) {
-	console.debug("DEBUG:" + data);
+function getWord(word) {
+	return lestWords[word];
 }
 
-function getAttr(id, attr) {
-	console.debug('Get in:' + id + '/' + attr);
-	var elem = document.getElementById(id);
-	return (elem[attr]);
+function toParams(data) {
+	console.debug(typeof data);
+	var out = new Array();
+	for (key in data) {
+		console.debug(key, encodeURIComponent(data[key]));
+	    out.push(key + '=' + encodeURIComponent(data[key]));
+	}
+	return out.join('&');
 }
 
-function setAttr(id, attr, data) {
-	var elem = document.getElementById(id);
-	elem[attr] = data;
-}
-
-
-function action(act,data,target) {
-	/* TODO: Add support for data */
-	var reply = sendAction(act);
-	setContent(target,reply);
-}
-
-function sendAction(act) {
+function send(act, data) {
  	var reg = new XMLHttpRequest();
- 	act = 'index.r3?action=' + act;
- 	reg.open('GET', act, false);
+ 	var act = "index.r3?action=" + act;
+ 	reg.open("GET", act, false);
+	reg.send(data);
+	var res = reg.response; 
+	return res;
+}
+
+function sendAction(act, data) {
+ 	console.debug("OACT");
+ 	console.debug("1ACT:" + act);
+ 	var reg = new XMLHttpRequest();
+ 	var act = "index.r3?action=" + act + "&" + toParams(data);
+ 	console.debug("ACT:" + act);
+ 	reg.open("GET", act, false);
 	reg.send();
-	res = reg.response; 
+	var res = reg.response; 
+	console.debug("RES:" + res);
 	return res;
 }
 
 function setContent(id, data) {
 	var elem = document.getElementById(id);
 	elem.innerHTML = data;
+}
+
+function getAttr(id, attr) {
+	var elem = document.getElementById(id);
+	return (elem[attr]);
+}
+
+function setAttr(id, attr, data) {
+	var elem = document.getElementById(id);
+	console.debug("SET:" + elem.id + "/" + attr + "=" + data);
+	elem[attr] = data;
 }
 
 // watcher
